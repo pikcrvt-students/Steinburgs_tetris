490b2984a26598dbb32ba2c07fab40c06e810460
21DP1GDimi <106299275+21DP1GDimi@users.noreply.github.com>
2022-10-25 15:06:14 +0300

    Add files via upload

---
+++ SFML learning.cpp
@@ -0,0 +1,197 @@
+﻿/*Programmu veidoja: Gļebs Dimitrijevs DP2-1*/
+
+/*Importējam visas nepieciešamās bibliotēkas*/
+#include <SFML/Graphics.hpp>
+#include <time.h>
+
+using namespace sf;
+
+/*Konsoles augstums un platums*/
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = { 0 };
+
+/*Kvadrātiņu lielums*/
+int w = 34;
+
+struct Point
+{
+    int x, y;
+}
+a[4], b[4];
+
+/*masīvs ar figūrām, 7 figuras, 4 kvadrati katrā*/
+int figures[7][4] = {
+    1,3,5,7,
+    2,4,5,7,
+    3,5,4,6,
+    3,5,4,7,
+    2,3,5,7,
+    3,5,7,6,
+    2,3,4,5,
+};
+
+
+bool check() {
+    for (int i = 0; i < 4; i++)
+        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M)
+            return 0;
+        else if (field[a[i].y][a[i].x])
+            return 0;
+
+    return 1;
+}
+/*Main funkcija*/
+int main()
+{
+    /*Pierakstam laiku*/
+    srand(time(0));
+
+    /*Pats logs*/
+    RenderWindow window(VideoMode(N * w, M * w), "Tetris!");
+
+    /*Bildes mainīgais*/
+    Texture t;
+
+    /*Nosakam ceļu līdz mums nepiecešamai bildei*/
+    t.loadFromFile("C:\\Users\\dimit\\Documents\\libraries\\tiles.png");
+
+    /*Spēles objekts, struktūra atkarīga no importētās bildes*/
+    Sprite tiles(t);
+
+
+    /*Mainīgie, kas ļauj kustināt figūru horizontāli*/
+    int dx = 0, colorNum = 1;
+    bool rotate = false;
+    float timer = 0, delay = 0.3;
+    Clock clock;
+    bool ad = true;
+
+    /*Main cikls*/
+    while (window.isOpen())
+    {
+        
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event event;
+        while (window.pollEvent(event))
+        {
+            /*Atbild par loga aizvēršanu*/
+            if (event.type == Event::Closed)
+                window.close();
+
+            /*Pogas, kas atbild par figūras kustību un rotēšanu*/
+            if (event.type == Event::KeyPressed)
+                if (event.key.code == Keyboard::Up)
+                    rotate = true;
+                else if (event.key.code == Keyboard::Right)
+                    dx = 1;
+                else if (event.key.code == Keyboard::Left)
+                    dx = -1;
+        }
+
+        /*Paatrina kustību uz leju*/
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            delay = 0.05;
+
+        for (int i = 0; i < 4; i++) {
+            b[i] = a[i];
+            a[i].x += dx;
+        }
+
+        if (!check()) {
+            for (int i = 0; i < 4; i++)
+                a[i] = b[i];
+        }
+
+        /*Rotējam figūru*/
+        if (rotate) {
+            Point p = a[1];
+            for (int i = 0; i < 4; i++) {
+                int x = a[i].y - p.y;
+                int y = a[i].x - p.x;
+
+                a[i].x = p.x - x;
+                a[i].y = p.y + y;
+            }
+
+            if (!check()) {
+                for (int i = 0; i < 4; i++)
+                    a[i] = b[i];
+            }
+        }
+
+        if (timer > delay) {
+            for (int i = 0; i < 4; i++) {
+                b[i] = a[i];
+                a[i].y += 1;
+            }
+
+            if (!check()) {
+                for (int i = 0; i < 4; i++)
+                    field[b[i].y][b[i].x] = colorNum;
+                colorNum = 1 + rand() % 7;
+                int n = rand() % 7;
+                for (int i = 0; i < 4; i++) {
+                    a[i].x = figures[n][i] % 2;
+                    a[i].y = figures[n][i] / 2;
+                }
+            }
+
+            timer = 0;
+        }
+
+        if (ad) {
+            int n = rand() % 7;
+            if (a[0].x == 0)
+                for (int i = 0; i < 4; i++) {
+                    a[i].x = figures[n][i] % 2;
+                    a[i].y = figures[n][i] / 2;
+                }
+            ad = false;
+        }
+
+        /*Kad aizpildās līnija, tā pazūd*/
+        int k = M - 1;
+        for (int i = M - 1; i > 0; i--) {
+            int count = 0;
+            for (int j = 0; j < N; j++) {
+                if (field[i][j])
+                    count++;
+                field[k][j] = field[i][j];
+            }
+            if (count < N)
+                k--;
+        }
+
+        dx = 0;
+        rotate = false;
+        delay = 0.3;
+
+        /*Uzliekam baltu fonu*/
+        window.clear(Color::White);
+
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++) {
+                if (field[i][j] == 0)
+                    continue;
+                tiles.setTextureRect(IntRect(field[i][j] * w, 0, w, w));
+                tiles.setPosition(j * w, i * w);
+                window.draw(tiles);
+            }
+
+        /*For cikls, kas atbild par figūrām un tas izvadīšanu logā*/
+        for (int i = 0; i < 4; i++) {
+            tiles.setTextureRect(IntRect(colorNum * w, 0, w, w));
+            tiles.setPosition(a[i].x * w, a[i].y * w);
+            /*Komanda, kura izvada figūru*/
+            window.draw(tiles);
+        }
+
+        window.display();
+    }
+    return 0;
+}
